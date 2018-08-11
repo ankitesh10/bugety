cd496f7da3afa95f048623988bd5612c6f7c90d6
aroraankitesh <ankitesh4@gmail.com>
2018-08-11 16:22:39 +0530

    Adding event Listener

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 var budgetController = (function(){
 
-    var x = 23;
-    var add =  function(a){
-        return x+a;
-    }
-
-    return{
-        publicTest: function(b){
-            return add(b);
-        }
-    }
+  
+    
 })();
 
 
@@ -22,12 +14,30 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
 
-    return{
-        anotherPublic: function(){
-            console.log(z);
+    var ctrlAddItem = function(){
+        // 1. Get the Field Input Data
+        
+            // 2. Add the item to the budget Controller
+        
+            // 3. Add the item to the UI
+        
+            // 4. Calculate the budget
+        
+            // 5. Display the budget on the UI
+
+            console.log('It Works')
         }
+
+   document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+   document.addEventListener('keypress', function(event){
+    
+    if(event.keyCode === 13 || event.which === 13){
+
+        ctrlAddItem();
     }
+    
+   });
 
 })(budgetController, UIController);
