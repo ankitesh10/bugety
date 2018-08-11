b17ec6b7981965eb1f68d8fbb6227a8e065cf8e6
aroraankitesh <ankitesh4@gmail.com>
2018-08-11 17:27:22 +0530

    fetching value from html structuring Code

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function(){
-
-  
-    
-})();
-
-
-var UIController = (function(){
 
     // some code
 
-});
+})();
 
 
+
+// UI CONTROLLER
+var UIController = (function(){
+
+    var DOMstrings = {
+        inputType : '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+
+
+        return{
+            getInput: function(){
+                return{
+                    type: document.querySelector(DOMstrings.inputType).value,   //value will either be inc or exp
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value 
+                };
+            },
+
+            getDomStrings : function() {
+                return DOMstrings;
+            }
+
+        }
+})();
+
+
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    DOM = UICtrl.getDomStrings();
 
     var ctrlAddItem = function(){
         // 1. Get the Field Input Data
-        
+
+        var input =  UICtrl.getInput();
+        console.log(input);       
             // 2. Add the item to the budget Controller
         
             // 3. Add the item to the UI
@@ -26,10 +56,9 @@ var controller = (function(budgetCtrl, UICtrl){
         
             // 5. Display the budget on the UI
 
-            console.log('It Works')
         }
 
-   document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+   document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
 
    document.addEventListener('keypress', function(event){
     
